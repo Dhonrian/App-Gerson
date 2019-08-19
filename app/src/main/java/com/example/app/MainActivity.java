1c3eb11df64ff2cc42aaac5079340be082fa2354
leo <leonardo.tamanhao@gmail.com>
2019-08-18 23:11:22 -0300

    Aplicativo

---
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        changeScreen();
+    }
+
+    private void changeScreen(){
+
+
+        Button btn = (Button) findViewById(R.id.search);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, informationActivity.class));
+            }
+        });
+
+    }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
