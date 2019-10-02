a694b2fb9c78fae32e925cf7d6c29ea578b2ab84
leo <leonardo.tamanhao@gmail.com>
2019-10-02 20:28:11 -0300

    Mapa quase funcionando

--- app/src/main/java/com/example/app/informationActivity.java
+++ app/src/main/java/com/example/app/informationActivity.java
@@ -7,7 +7,9 @@ import android.location.Geocoder;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -15,9 +17,11 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
-public class informationActivity extends AppCompatActivity {
+public class informationActivity extends AppCompatActivity implements OnMapReadyCallback {
     private Geocoder geocoder = null;
 
+    private MapView mMapView;
+    public static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,6 +32,16 @@ public class informationActivity extends AppCompatActivity {
         TextView text = (TextView) findViewById(R.id.teste);
         TextView end = (TextView) findViewById(R.id.end);
         TextView time = (TextView) findViewById(R.id.tempo);
+        MapView mMapView = (MapView) findViewById(R.id.mapView);
+
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
+        }
+
+        mMapView.onCreate(mapViewBundle);
+
+        mMapView.getMapAsync(this);
 
         geocoder = new Geocoder(this, Locale.getDefault());
         SimpleDateFormat currentTime = new SimpleDateFormat("dd/MM/yyyy '-' HH:mm:ss");
@@ -37,7 +51,6 @@ public class informationActivity extends AppCompatActivity {
         String roubado = isRoubado(pl);
         text.setText("Placa: "+ pl.getPlaca()+ "\n" + "Cor: " + pl.getCor()+  "\n" + "Roubado: " + roubado);
         List<Address> addresses = null;
-
 
         try {
             addresses = geocoder.getFromLocation(pl.getLat(), pl.getLon(), 1);
@@ -60,4 +73,29 @@ public class informationActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.setMyLocationEnabled(true);
+    }
+
+    @Override
+    public void onPause() {
+        mMapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        mMapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
 }
+
+
+
